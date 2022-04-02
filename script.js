59ba53673cbcfbc62c98b644e46e533ea18757dc
Mehmet Ulga <mehmetulga@gmail.com>
2022-04-02 18:03:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
-// YUZDE 5
+
+const bill = document.getElementById("bill");
+const person = document.getElementById("person");
+const tipAmount = document.getElementById("tip_amount");
+const totalWrite = document.getElementById("total");
+const five = document.getElementById("five");
+
+// YUZDE 5 
+
 
 document.getElementById("five").onclick = function () {
-  const bill = document.getElementById("bill").value;
-  const person = document.getElementById("person").value;
-  yuzde5 = (bill * 0.05) / person;
-  total = bill * 0.05;
-  document.getElementById("tip_amount").innerHTML = yuzde5.toFixed(2);
-  document.getElementById("total").innerHTML = total.toFixed(2);
+  yuzde5 = (bill.value * 0.05) / person.value;
+  total = bill.value * 0.05;
+  tipAmount.innerHTML = yuzde5.toFixed(2);
+  totalWrite.innerHTML = total.toFixed(2);
   console.log(yuzde5);
 };
 
 // YUZDE 10 
 document.getElementById("ten").onclick = function () {
-  const bill = document.getElementById("bill").value;
-  const person = document.getElementById("person").value;
-  yuzde5 = (bill * 0.1) / person;
-  total = bill * 0.1;
-  document.getElementById("tip_amount").innerHTML = yuzde5.toFixed(2);
-  document.getElementById("total").innerHTML = total.toFixed(2);
-  console.log(yuzde5);
+  yuzde10 = (bill.value * 0.1) / person.value;
+  total = bill.value * 0.1;
+  tipAmount.innerHTML = yuzde10.toFixed(2);
+  totalWrite.innerHTML = total.toFixed(2);
+  console.log(yuzde10);
 };
 
 // YUZDE 15 
 document.getElementById("fifteen").onclick = function () {
-  const bill = document.getElementById("bill").value;
-  const person = document.getElementById("person").value;
-  yuzde5 = (bill * 0.15) / person;
-  total = bill * 0.15;
-  document.getElementById("tip_amount").innerHTML = yuzde5.toFixed(2);
-  document.getElementById("total").innerHTML = total.toFixed(2);
-  console.log(yuzde5);
+  yuzde15 = (bill.value * 0.15) / person.value;
+  total = bill.value * 0.15;
+  tipAmount.innerHTML = yuzde15.toFixed(2);
+  totalWrite.innerHTML = total.toFixed(2);
+  console.log(yuzde15);
 };
 
-// YUZDE 25 
+// YUZDE 25 ;
 document.getElementById("twenty_five").onclick = function () {
-  const bill = document.getElementById("bill").value;
-  const person = document.getElementById("person").value;
-  yuzde5 = (bill * 0.25) / person;
-  total = bill * 0.25;
-  document.getElementById("tip_amount").innerHTML = yuzde5.toFixed(2);
-  document.getElementById("total").innerHTML = total.toFixed(2);
-  console.log(yuzde5);
+  yuzde25 = (bill.value * 0.25) / person.value;
+  total = bill.value * 0.25;
+  tipAmount.innerHTML = yuzde25.toFixed(2);
+  totalWrite.innerHTML = total.toFixed(2);
+  console.log(yuzde25);
 };
 
-// YUZDE 50 
+// YUZDE 50 ;
 document.getElementById("fifty").onclick = function () {
-  const bill = document.getElementById("bill").value;
-  const person = document.getElementById("person").value;
-  yuzde5 = (bill * 0.5) / person;
-  total = bill * 0.5;
-  document.getElementById("tip_amount").innerHTML = yuzde5.toFixed(2);
-  document.getElementById("total").innerHTML = total.toFixed(2);
-  console.log(yuzde5);
+  yuzde50 = (bill.value * 0.5) / person.value;
+  total = bill.value * 0.5;
+  tipAmount.innerHTML = yuzde50.toFixed(2);
+  totalWrite.innerHTML = total.toFixed(2);
+  console.log(yuzde50);
 };
