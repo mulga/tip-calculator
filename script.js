67c8436109c8bc958171828eb1ae95068fe4aa42
Mehmet Ulga <mehmetulga@gmail.com>
2022-04-03 10:49:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.getElementById("five").onclick = function () {
   total = bill.value * 0.05;
   tipAmount.innerHTML = yuzde5.toFixed(2);
   totalWrite.innerHTML = total.toFixed(2);
-  console.log(yuzde5);
+  
 };
 
 // YUZDE 10 
@@ -22,7 +22,7 @@ document.getElementById("ten").onclick = function () {
   total = bill.value * 0.1;
   tipAmount.innerHTML = yuzde10.toFixed(2);
   totalWrite.innerHTML = total.toFixed(2);
-  console.log(yuzde10);
+  
 };
 
 // YUZDE 15 
@@ -31,10 +31,10 @@ document.getElementById("fifteen").onclick = function () {
   total = bill.value * 0.15;
   tipAmount.innerHTML = yuzde15.toFixed(2);
   totalWrite.innerHTML = total.toFixed(2);
-  console.log(yuzde15);
+  
 };
 
-// YUZDE 25 ;
+// YUZDE 25 
 document.getElementById("twenty_five").onclick = function () {
   yuzde25 = (bill.value * 0.25) / person.value;
   total = bill.value * 0.25;
@@ -43,13 +43,13 @@ document.getElementById("twenty_five").onclick = function () {
   console.log(yuzde25);
 };
 
-// YUZDE 50 ;
+// YUZDE 50 
 document.getElementById("fifty").onclick = function () {
   yuzde50 = (bill.value * 0.5) / person.value;
   total = bill.value * 0.5;
   tipAmount.innerHTML = yuzde50.toFixed(2);
   totalWrite.innerHTML = total.toFixed(2);
-  console.log(yuzde50);
+  
 };
 
 
